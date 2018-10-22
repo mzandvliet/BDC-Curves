d7720bccfb333f8d48569d3c8b0cabc7b123fcd5
m_zandvliet <martijn@ramjetanvil.com>
2018-10-22 21:08:29 +0200

    Implementation of cubic curves

--- Assets/Scripts/BDCCurve.cs
+++ Assets/Scripts/BDCCurve.cs
@@ -54,12 +54,13 @@ public class BDCCurve : MonoBehaviour {
 
 
 	private void Awake () {
-		_curve = new NativeArray<float3>(3, Allocator.Persistent);
-        _distanceCache = new NativeArray<float>(64, Allocator.Persistent);
+		_curve = new NativeArray<float3>(4, Allocator.Persistent);
+        _distanceCache = new NativeArray<float>(32, Allocator.Persistent);
 
-		_curve[0] = new float3(0f, 0f, 0f);
-        _curve[1] = new float3(2f, 0f, 0f);
-        _curve[2] = new float3(4f, 0f, 0f);
+		_curve[0] = new float3(-2f, 3f, 0f);
+        _curve[1] = new float3(-1f, 0.5f, 0f);
+        _curve[2] = new float3(1f, 0f, 0f);
+        _curve[3] = new float3(2f, 1f, 0f);
 
 		_rng = new Rng(1234);
 
@@ -91,11 +92,11 @@ public class BDCCurve : MonoBehaviour {
 	}
 	
 	private void Update () {
-		_curve[0] += new float3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0f) * Time.deltaTime * 10f;
+		// _curve[0] += new float3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0f) * Time.deltaTime * 10f;
 
-		Relax();
+		// Relax();
 
-		BDC3.CacheDistances(_curve[0], _curve[1], _curve[2], _distanceCache);
+		BDC3Cube.CacheDistances(_curve, _distanceCache);
 
         var j = new MakeMeshJob();
         j.verts = _verts;
@@ -108,27 +109,24 @@ public class BDCCurve : MonoBehaviour {
 		JobHandle.ScheduleBatchedJobs();
 	}
 
+    // Todo: this needs to be rewritten with 2 control points in the middle for the cubic
 	private void Relax() {
-		// Todo: I want the partial derivatives of the following with respect to _pos[1]
-		// But I can cheat for now
-
 		// First step: apply control point constraints
 
 		_curve[0] = ClipHeight(_curve[0], 0.05f, 2f);
 
 		var handleDelta = _curve[0] - _curve[2];
-		if (BDC3.Length(handleDelta) > 4f) {
+		if (math.length(handleDelta) > 4f) {
 			_curve[0] = _curve[2] + math.normalize(handleDelta) * 4f;
 		}
 
 		// Step 2, relax the curvature control to be above ground, and keep paper area constant
-		// Todo: momentum
 
 		int iters = 0;
 		float lengthError = 1f;
 		while (lengthError > 0.001f && iters < 64) {
 			int refinementSteps = 4 + iters / 16;
-            float length = BDC3.LengthEuclidApprox(_curve[0], _curve[1], _curve[2], refinementSteps);
+            float length = BDC3Cube.LengthEuclidApprox(_curve, refinementSteps);
 			float3 midPoint = (_curve[2] + _curve[0]) * 0.5f;
             float3 cord = math.normalize(_curve[2] - _curve[0]);
 			float3 perp = math.cross(cord, new float3(0f, 0f, 1f));
@@ -139,7 +137,6 @@ public class BDCCurve : MonoBehaviour {
 			_curve[1] += new float3(0f, -0.02f * math.abs(lengthError), 0f); // a gravity term
 
 			_curve[1] = ClipHeight(_curve[1], 0f, 4f);
-            // _curve[1] = StayLeftOf(_curve[1], _curve[2].x - 0.5f);
 
 			iters++;
 		}
@@ -168,12 +165,12 @@ public class BDCCurve : MonoBehaviour {
         }
 
 		Gizmos.color = Color.white;
-		float3 pPrev = BDC3.Evaluate(_curve[0], _curve[1], _curve[2], 0f);
+		float3 pPrev = BDC3Cube.Evaluate(_curve, 0f);
         Gizmos.DrawSphere(pPrev, 0.01f);
 		int steps = 16;
 		for (int i = 1; i <= steps; i++) {
 			float t = (i / (float)steps);
-			float3 p = BDC3.Evaluate(_curve[0], _curve[1], _curve[2], t);
+			float3 p = BDC3Cube.Evaluate(_curve, t);
 			Gizmos.DrawLine(pPrev, p);
             Gizmos.DrawSphere(p, 0.01f);
 			pPrev = p;
@@ -181,7 +178,7 @@ public class BDCCurve : MonoBehaviour {
 	}
 
 	private void OnGUI() {
-		GUILayout.Label("Length: " + BDC3.LengthEuclidApprox(_curve[0], _curve[1], _curve[2], 16));
+		GUILayout.Label("Length: " + BDC3Cube.LengthEuclidApprox(_curve, 16));
 	}
 
 	private void UpdateMesh() {
@@ -215,14 +212,14 @@ public class BDCCurve : MonoBehaviour {
                 var gridPos = Math.ToXZFloat(i, RES);
 				var gridPosUnit = gridPos / (float)(RES - 1);
 
-				var p = BDC3.Evaluate(curve[0], curve[1], curve[2], gridPosUnit.x);
+				var p = BDC3Cube.Evaluate(curve, gridPosUnit.x);
 				p.z = gridPosUnit.z * 4f;
 
-				normals[i] = BDC3.EvaluateNormalApprox(curve[0], curve[1], curve[2], gridPosUnit.x);
+				normals[i] = BDC3Cube.EvaluateNormal(curve, gridPosUnit.x, new float3(0,1,0));
                 verts[i] = p;
 
                 // Todo: this calculation can be cumulative along mesh, this is pretty wasteful
-                float uvx = BDC3.LengthEuclidApprox(distances, gridPosUnit.x) / distances[distances.Length-1];
+                float uvx = BDC3Cube.LengthEuclidApprox(distances, gridPosUnit.x) / distances[distances.Length-1];
                 uvs[i] = new float2(uvx, gridPosUnit.z);
 				
             }
@@ -310,7 +307,98 @@ public static class Util {
 
 }
 
-public static class BDC3 {
+// Borrowing from: https://www.youtube.com/watch?v=o9RK6O2kOKo
+public static class BDC3Cube {
+    public static float3 Lerp(float3 a, float3 b, float t) {
+        return t * a + (1f - t) * b;
+    }
+    public static float3 EvaluateCasteljau(NativeArray<float3> p, float t) {
+        float3 bc = Lerp(p[1],p[2],t);
+        return Lerp(Lerp(Lerp(p[0], p[1], t), bc, t), Lerp(bc, Lerp(p[2], p[3], t), t), t);
+    }
+
+    public static float3 Evaluate(NativeArray<float3> p, float t) {
+        float omt = 1f - t;
+        float omt2 = omt * omt;
+        float t2 = t * t;
+        return 
+            p[0] * (omt2 * omt) + 
+            p[1] * (3f * omt2 * t) +
+            p[2] * (3f * omt * t2) +
+            p[3] * (t2 * t);
+    }
+
+    public static float3 EvaluateTangent(NativeArray<float3> c, float t) {
+        float omt = 1f - t;
+        float omt2 = omt * omt;
+        float t2 = t * t;
+        return  math.normalize(
+            c[0] * (-omt2) +
+            c[1] * (3f * omt2 - 2f * omt) +
+            c[2] * (-3f * t2 + 2f * t) +
+            c[3] * (t2)
+        );
+    }
+
+    public static float3 EvaluateNormal(NativeArray<float3> c, float t, float3 up) {
+        float3 tangent = EvaluateTangent(c, t);
+        float3 binorm = math.cross(up, tangent);
+        return math.normalize(math.cross(tangent, binorm));
+    }
+
+    public static float LengthEuclidApprox(NativeArray<float3> c, int steps) {
+        float dist = 0;
+
+        float3 pPrev = c[0];
+        for (int i = 1; i <= steps; i++) {
+            float t = i / (float)steps;
+            float3 p = Evaluate(c, t);
+            dist += math.length(p - pPrev);
+            pPrev = p;
+        }
+
+        return dist;
+    }
+
+    public static float LengthEuclidApprox(NativeArray<float3> c, int steps, float t) {
+        float dist = 0;
+
+        float3 pPrev = c[0];
+        for (int i = 1; i <= steps; i++) {
+            float tNow = t * (i / (float)steps);
+            float3 p = Evaluate(c, tNow);
+            dist += math.length(p - pPrev);
+            pPrev = p;
+        }
+
+        return dist;
+    }
+
+    public static float LengthEuclidApprox(NativeArray<float> distances, float t) {
+        t = t * (float)(distances.Length - 1);
+        int ti = (int)math.floor(t);
+        if (ti >= distances.Length - 1) {
+            return distances[distances.Length - 1];
+        }
+        return math.lerp(distances[ti], distances[ti + 1], t - (float)ti);
+    }
+
+    // Instead of storing at linear t spacing, why not store with non-linear t-spacing and lerp between them
+    public static void CacheDistances(NativeArray<float3> c, NativeArray<float> outDistances) {
+        float dist = 0;
+        outDistances[0] = 0f;
+        float3 pPrev = c[0];
+        for (int i = 1; i < outDistances.Length; i++) {
+            float t = i / (float)(outDistances.Length - 1);
+            float3 p = Evaluate(c, t);
+            dist += math.length(p - pPrev);
+            outDistances[i] = dist;
+            pPrev = p;
+        }
+    }
+}
+
+public static class BDC3Quad {
     public static float Length(float3 v) {
         return math.sqrt(v.x * v.x + v.y * v.y + v.z * v.z);
     }
@@ -339,10 +427,10 @@ public static class BDC3 {
     public static float LengthEuclidApprox(float3 a, float3 b, float3 c, int steps) {
         float dist = 0;
 
-        float3 pPrev = BDC3.Evaluate(a, b, c, 0f);
+        float3 pPrev = Evaluate(a, b, c, 0f);
         for (int i = 1; i <= steps; i++) {
             float t = i / (float)steps;
-            float3 p = BDC3.Evaluate(a, b, c, t);
+            float3 p = Evaluate(a, b, c, t);
             dist += Length(p - pPrev);
             pPrev = p;
         }
@@ -353,10 +441,10 @@ public static class BDC3 {
     public static float LengthEuclidApprox(float3 a, float3 b, float3 c, int steps, float t) {
         float dist = 0;
 
-        float3 pPrev = BDC3.Evaluate(a, b, c, 0f);
+        float3 pPrev = Evaluate(a, b, c, 0f);
         for (int i = 1; i <= steps; i++) {
             float tNow = t * (i / (float)steps);
-            float3 p = BDC3.Evaluate(a, b, c, tNow);
+            float3 p = Evaluate(a, b, c, tNow);
             dist += Length(p - pPrev);
             pPrev = p;
         }
@@ -377,10 +465,10 @@ public static class BDC3 {
 	public static void CacheDistances(float3 a, float3 b, float3 c, NativeArray<float> outDistances) {
         float dist = 0;
 		outDistances[0] = 0f;
-        float3 pPrev = BDC3.Evaluate(a, b, c, 0f); // Todo: this is just point a
+        float3 pPrev = Evaluate(a, b, c, 0f); // Todo: this is just point a
 		for (int i = 1; i < outDistances.Length; i++) {
             float t = i / (float)(outDistances.Length-1);
-            float3 p = BDC3.Evaluate(a, b, c, t);
+            float3 p = Evaluate(a, b, c, t);
             dist += Length(p - pPrev);
 			outDistances[i] = dist;
             pPrev = p;
